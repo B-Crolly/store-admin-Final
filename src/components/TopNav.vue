7bcb683c065683d14754848dc9d37ac043f85d46
bill <bcrolly@gmail.com>
2024-12-12 23:04:40 -0500

    modified

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,8 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <!--test-->
+        <img src="@/assets/best-buy-logo.jpg" alt="Best Buy Logo"> 
       </a>
       Admin Portal
     </div>
@@ -40,7 +41,7 @@ nav {
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: #333;
+  background-color: #0046be;
   color: #fff;
   padding-top: 0.5rem;
   padding-left: 1rem;
